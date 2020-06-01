12b4b19ece473705c863b0d2aa0b087828153084
Brennen Tatum <bmar566@gmail.com>
2020-06-01 10:06:10 -0500

    finished Part 1 of Bank Heist exercise

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace bankHeist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Team 4!!");
+            Console.WriteLine("Plan your Heist!!");
+            Console.Write("Enter Team Member Name: ");
+            string TeamMemberName = Console.ReadLine();
+
+            Console.Write("Enter Team Member skill level 1- 10: ");
+            int TeamMemberSkillLevel = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Team Member Courage Level 0.0 - 2.0: ");
+            double TeamMemberCourage = double.Parse(Console.ReadLine());
+            TeamMember member1 = new TeamMember()
+            {
+                Name = TeamMemberName,
+                SkillLevel = TeamMemberSkillLevel,
+                Courage = TeamMemberCourage
+
+            };
+            member1.GetMemberDetails();
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace bankHeist
+{
+    class TeamMember
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+
+        public double Courage { get; set; }
+
+        public void GetMemberDetails()
+        {
+            Console.WriteLine("-----------------");
+            Console.WriteLine($"Team Member: {Name}");
+            Console.WriteLine($"Member Skill Level: {SkillLevel}");
+            Console.WriteLine($"Courage Level: {Courage}");
+            Console.WriteLine("-----------------");
+        }
+
+    }
+
+}
